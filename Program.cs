3e45aa79d642f8f5f5a13245d53f4799875b4211
Archibasov2023 <arch1405@yandex.ru>
2023-03-26 19:23:48 +0400

    First comm

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿// Написать программу, которая из имеюшегося
+// массива строк формирует массив из строк,
+// длина которых <=3 символа.
+
+void MethodCreateInputArray(string[] InputArray)
+{
+    Console.Write($"Input array: ");
+    for (int i = 0; i < InputArray.Length; i++) Console.Write($"{InputArray[i]} ");
+    Console.WriteLine();
+}
+
+
